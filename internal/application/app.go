2be5bdbf8f5d32f985763b91f49566f1f4d7ab60
artarts36 <temicska99@mail.ru>
2026-06-20 15:22:04 +0300

    refactor: rename secrets package to sync

--- internal/application/app.go
+++ internal/application/app.go
@@ -17,7 +17,7 @@ import (
 	"github.com/swarm-deploy/cloud-secrets/internal/metrics"
 	"github.com/swarm-deploy/cloud-secrets/internal/providers/cloudru"
 	"github.com/swarm-deploy/cloud-secrets/internal/providers/contracts"
-	"github.com/swarm-deploy/cloud-secrets/internal/secrets"
+	"github.com/swarm-deploy/cloud-secrets/internal/sync"
 )
 
 type Application struct {
@@ -31,7 +31,7 @@ type Application struct {
 
 	docker dock.APIClient
 
-	synchronizer *secrets.Synchronizer
+	synchronizer *sync.Synchronizer
 
 	synchronizing atomic.Bool
 }
@@ -58,7 +58,7 @@ func NewApplication(ctx context.Context, cfg config.Config, metricsGroup *metric
 
 	app.secretProvider = contracts.WithMetrics(provider, metricsGroup.Provider)
 
-	app.synchronizer = secrets.NewSynchronizer(
+	app.synchronizer = sync.NewSynchronizer(
 		engine.NewDockerClient(dockerClient, metricsGroup.Docker),
 		provider,
 		metricsGroup.Secrets,
