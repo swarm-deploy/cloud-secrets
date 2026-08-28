cb675b73164b227c72b324726fc123e170f674c0
artarts36 <temicska99@mail.ru>
2026-08-28 21:02:37 +0300

    chore: rename init command to vault

--- internal/application/cli/init.go
+++ internal/application/cli/init.go
@@ -2,48 +2,73 @@ package cli
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/swarm-deploy/cloud-secrets/internal/application/cli/framework"
-	"github.com/swarm-deploy/cloud-secrets/internal/config"
 	"github.com/swarm-deploy/cloud-secrets/internal/engine"
 )
 
-type InitCommand struct {
+const (
+	vaultAuthMethodTokenStatic = iota
+)
+
+var vaultAuthMethods = []string{
+	"Token (static)",
+}
+
+type VaultCommand struct {
 	docker engine.Client
 }
 
-func NewInitCommand(docker engine.Client) *InitCommand {
-	return &InitCommand{
+func NewInitCommand(docker engine.Client) *VaultCommand {
+	return &VaultCommand{
 		docker: docker,
 	}
 }
 
-func (c *InitCommand) Definition() framework.Definition {
+func (c *VaultCommand) Definition() framework.Definition {
 	return framework.Definition{
-		Name:        "init",
-		Description: "Prepare cluster to working with cloud-secrets",
+		Name:        "vault",
+		Description: "Prepare Swarm and Vault to working with cloud-secrets",
 	}
 }
 
-func (c *InitCommand) Run(ctx context.Context, exec *framework.Execution) error {
-	secretProviderIndex, err := exec.AskWithChoices("Choose your secret provider", config.ProviderNames)
+func (c *VaultCommand) Run(ctx context.Context, exec *framework.Execution) error {
+	authMethod, err := exec.AskWithChoices("Choose auth method", vaultAuthMethods)
 	if err != nil {
 		return err
 	}
 
-	if secretProviderIndex < 0 || secretProviderIndex >= len(config.ProviderNames) {
-		return fmt.Errorf("got invalid provider index %d", secretProviderIndex)
+	switch authMethod { //nolint:gocritic // nn
+	case vaultAuthMethodTokenStatic:
+		return c.initVaultTokenStatic(ctx, exec)
 	}
 
-	secretProviderName := config.ProviderNames[secretProviderIndex]
+	return nil
+}
 
-	switch secretProviderName {
-	case string(config.ProviderNameCloudRU):
-		return nil
-	case string(config.ProviderNameVault):
-		return c.initVault(ctx, exec)
+func (c *VaultCommand) initVaultTokenStatic(ctx context.Context, exec *framework.Execution) error {
+	token, err := exec.AskPassword("Enter static token for cloud-secrets")
+	if err != nil {
+		return err
 	}
+
+	confirmed := exec.Confirm("Now we'll create a secret in Docker Swarm that will be used by the cloud-secrets service. The secret will be named `cloud-secrets-vault-token`") //nolint:lll // nn
+	if !confirmed {
+		return errors.New("operation not confirmed")
+	}
+
+	err = c.docker.CreateSecret(ctx, engine.CreatingSecret{
+		Path:         "cloud-secrets-vault-token",
+		Value:        []byte(token),
+		ExternalPath: "cloud-secrets/vault-token",
+	})
+	if err != nil {
+		return fmt.Errorf("create secret in Docker Swarm: %w", err)
+	}
+
+	exec.PrintSuccess("The secret has been created. In the future, you can add a secret named \"cloud-secrets-vault-token\" to Vault and update the token directly from Vault.") //nolint:lll // nn
 
 	return nil
 }
